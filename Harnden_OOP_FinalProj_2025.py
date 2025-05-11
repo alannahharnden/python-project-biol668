f78fd80e1437a45388f992144f9abbf6d8881b2c
alannahharnden <138931442+alannahharnden@users.noreply.github.com>
2025-05-10 19:47:05 -0700

    Update Harnden_OOP_FinalProj_2025.py
    updated code with doctest, copied from fixed code file.

--- Harnden_OOP_FinalProj_2025.py
+++ Harnden_OOP_FinalProj_2025.py
@@ -1,4 +1,3 @@
-
 ## RENAME this file YourLastName_OOP_FinalProject_2025.py
 ## by Alannah Harnden
 
@@ -67,6 +66,39 @@
 #  (3) Add mol_weight, which returns the total molecular weight of the protein
 #      sequence assigned to the protein object.
 
+
+### Instance examples to attempt for various classes:
+
+#x=DNA("G","tmp","m",000)
+
+
+#pro=Protein(" WCVALKKKCCYhhhhh-yyyrsQ\t","my_protein","D. melanogaster", "56008009")
+#print(pro)
+#print(pro.fasta())
+#print(pro.kmers)
+#pro.kmers = []
+#pro.make_kmers(5)
+#print(pro.kmers)
+
+#testp=Protein('VIKING','test','unknown','999')
+#print(testp)
+#testp.make_kmers(2)
+#print(testp.kmers)
+#x=testp.total_hydro()
+#print(x)
+#m=testp.mol_weight()
+#print(m)
+
+#pro=Protein(" WCVALKKKCCYhhhhh-yyyrsQ\t","my_protein","D. melanogaster", "56008009")
+#print(pro)
+#print(pro.fasta())
+#print(pro.kmers)
+#pro.kmers = []
+#pro.make_kmers(5)
+#print(pro.kmers)
+
+### End of examples of instances
+
 import re
 
 standard_code = {
@@ -93,25 +125,39 @@ aa_mol_weights={'A':89.09,'C':121.15,'D':133.1,'E':147.13,'F':165.19,
                 'S':105.09,'T':119.12,'V':117.15,'W':204.23,'X':0,'Y':181.19}
     
 
-#x=DNA("G","tmp","m",000)
 
 
 #Seq Class.
 
 class Seq:
-    def __init__(self, sequence, gene, species, kmers=[]):
+    def __init__(self, sequence, gene, species):
         self.sequence = sequence.upper().strip()
         self.gene = gene
         self.species = species
-        self.kmers = kmers
+        self.kmers = []
 
     def print_record(self):
+        """This function returns the self.sequence as a string
+        >>> zak = Seq("CATAGAA", "zakis_gene", "Vombatus ursinus")
+        >>> print(zak.print_record())
+        CATAGAA
+        """
+        
         return self.sequence
 
-    def __str__(self):
+   def __str__(self):
         return self.species + ", " + self.gene + ": " + self.sequence
 
     def make_kmers(self, k=3):
+        """This function creates kmers from the provided sequence, with a length
+            of three each, moving by one, and appending them to a list self.kmers,
+            a parameter within the constructor. It ends when there is no further 3-unit
+            kmers to form of the sequence.
+        >>> khers = Seq("ACTAACCACA", "khers_gene", "Ursus arctos")
+        >>> print(khers.make_kmers())
+        ['ACT', 'CTA', 'TAA', 'AAC', 'ACC', 'CCA', 'CAC', 'ACA']
+        """
+        
         self.kmers=[]
         for i in range(0, len(self.sequence), 1):
             kmer = self.sequence[i:i+k]
@@ -122,6 +168,14 @@ class Seq:
         return self.kmers
 
     def fasta(self):
+        """This function returns the fasta title format of self.species, self.gene, followed
+        by self.sequence, as a string.
+        >>> asb = Seq("ACCATAAT", "asb_gene", "Equus caballus")
+        >>> print(asb.fasta())
+        >Equus caballus asb_gene
+        ACCATAAT
+        """
+        
         fasta_title = ">" + self.species + " " + self.gene + "\n" + self.sequence
         return fasta_title
     
@@ -133,8 +187,15 @@ class DNA(Seq):
         super().__init__(sequence, gene, species)
         self.gene_id = gene_id
         self.sequence = re.sub('[^ATGCU]','N',self.sequence)
+
+   def analysis(self):
+        """This function returns the count of Gs and Cs within a given DNA sequence, as
+        an integer.
+        >>> ghul = DNA("GCGCAAACGCG", "ghul_gene", "Chinese hibiscus", "CHB")
+        >>> print(ghul.analysis())
+        8
+        """
         
-    def analysis(self):
         gc_count = 0
         for base in self.sequence:
             if base == 'G' or base == 'C':
@@ -142,9 +203,24 @@ class DNA(Seq):
         return gc_count
 
     def print_info(self):
+        """This function returns a string entry of printed information, including the gene id
+        followed by the sequence.
+        >>> ghul = DNA("GCGCAAACGCG", "ghul_gene", "Chinese hibiscus", "CHB")
+        >>> print(ghul.print_info())
+         CHB GCGCAAACGCG
+        """
+        
         return  " " + self.gene_id +" " + self.sequence
 
     def reverse_complement(self):
+        """This function reverses the sequence to generate a reverse complement, as a string.
+        It first reverses the sequence and then replaces all U's with T's, as to clean up the
+        DNA sequence. It then converts each base to its complement nucleotide.
+        >>> ordaak = DNA("UACTAC", "ordaak_gene", "Anas platyrhynchos", "ANPLA")
+        >>> print(ordaak.reverse_complement())
+        CATCAT
+        """
+        
         self.sequence = self.sequence[::-1]
         self.sequence = self.sequence.replace('U','T')
         for base in self.sequence:
@@ -158,7 +234,15 @@ class DNA(Seq):
                 self.sequence.replace('T', 'A')
         return self.sequence
 
-    def six_frames(self):
+def six_frames(self):
+        """This function returns the first six frames of the sequence -- the first three
+        being in the 5'3 direction, and the last 3 in the 3'5 direction. These are returned
+        as a list of strings.
+        >>> bezorg = DNA("ACCNUATTGCAAAA", "big_gene", "Bezorgious bezo", "BZ")
+        >>> print(bezorg.six_frames())
+        ['ACCNUATTGCAAAA', 'CCNUATTGCAAAA', 'CNUATTGCAAAA', 'AAAACGTTATNCCA', 'AAACGTTATNCCA', 'AACGTTATNCCA']
+        """
+        
         frames = []
         f_frame_count = 0  
         for i in range(3):
@@ -182,7 +266,6 @@ class DNA(Seq):
             frames.append(frame)
         return frames
 
-
 #RNA Class.
 
 class RNA(DNA):
@@ -192,6 +275,13 @@ class RNA(DNA):
         self.codons = codons
 
     def make_codons(self):
+        """This function creates a list of a string of 3-base codons of a provided string RNA
+        sequence.
+        >>> parwana = RNA("TACACACAT", "parwana_gene", "Morpho menelaus", "MM")
+        >>> print(parwana.make_codons())
+        ['UAC', 'ACA', 'CAU']
+        """
+        
         self.codons=[]
         for i in range(0,len(self.sequence),3):
             codon = self.sequence[i:i+3]
@@ -202,6 +292,16 @@ class RNA(DNA):
         return self.codons
 
     def translate(self):
+        """This function translates the list of codons, previously generated from make_codons,
+        into a single protein string. It decodes the protein string utilizing the standard_code
+        dictionary to find the encoded amino acids.
+        >>> parwana = RNA("TACACACAT", "parwana_gene", "Morpho menelaus", "MM")
+        >>> parwana.make_codons()
+        ['UAC', 'ACA', 'CAU']
+        >>> print(parwana.translate())
+        YTH
+        """
+        
         self.codons = self.codons
         protein=''
         for codon in self.codons:
@@ -210,32 +310,50 @@ class RNA(DNA):
             protein+= aa
         return protein
 
-
-#Protein Class.
+  #Protein Class.
     
 class Protein(Seq):
-    def __init__(self, sequence, gene, species, kmers=[]):
-        super().__init__(sequence,gene,species,kmers)
-        self.sequence = sequence.upper().strip()
-        self.gene = gene
-        self.species = species
-        self.kmers = kmers
+    def __init__(self, sequence, gene, species, gene_id):
+        super().__init__(sequence, gene, species)
         self.sequence = re.sub('[^A-Za-z]','X',self.sequence)
 
 
     def total_hydro(self):
+        """This function calculates the hydrophobicity score for a given protein sequence. Pulls
+        individual hydrophobiicty scores per amino acids from the Kyle Doolittle dictionary. Returns
+        an entire string 'Hyrophobicity score:' followed by the string format of the integer score.
+        >>> ghalb = Protein("AXMRIQAIWAFBZA", "ghalb_gene", "Dicentra formosa", "DF")
+        >>> ghalb.total_hydro()
+        'Hydrophobicity score: 17.6'
+        """
+        
         total_score = 0
         for i in self.sequence:
             try:hydro_score = kyte_doolittle[i]
-                total_score+= hydro_score
             except: "?"
+            total_score+= hydro_score
         return "Hydrophobicity score:" + " " +str(total_score)
 
     def mol_weight(self):
+        """This function computes the molecular weight of a provided protein sequence. Similarly,
+        it utilizes a dictionary, but of molecular weights per amino acid compounds. The function
+        sums up these indivdual weights to calculate overall molecular weight. Returns a single
+        line, as a string "Total molecular weight:" followed by the value of the molecular weight
+        as a string.
+        >>> ghalb = Protein("AXMRIQAIWAFBZA", "ghalb_gene", "Dicentra formosa", "DF")
+        >>> ghalb.mol_weight()
+        'Total molecular weight: 1788.06'
+        """
+        
         total_weight = 0
         for i in self.sequence:
             try:weight = aa_mol_weights[i]
-                total_weight+= weight
             except: "X"
+            total_weight+= weight
         return "Total molecular weight:" + " " +str(total_weight)
-      
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
